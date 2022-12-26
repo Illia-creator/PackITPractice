d0d8e6aa7661eca212e402663bf3253745409437
Sakuuga <bezkotaneto4@gmail.com>
2022-12-26 19:20:13 +0200

    add entity

---
+++ src/PackITPractice.Domain/Entities/PackingList.cs
@@ -0,0 +1,17 @@
+﻿namespace PackITPractice.Domain.Entities
+{
+    public class PackingList
+    {
+        public Guid Id { get; private set; }
+
+        private string name;
+        private string localization;
+
+        internal PackingList(Guid id, string name, string localization)
+        {
+            this.Id = id;
+            this.name = name;
+            this.localization = localization;
+        }
+    }
+}
