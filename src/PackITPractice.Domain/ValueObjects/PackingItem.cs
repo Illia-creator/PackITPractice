8c59a3ff06d9a8bf7eb38eae2b99ba7f64bf3b26
Illia-creator <sie29092002@gmail.com>
2022-12-29 15:07:15 +0200

    is feat: add exceptions and value objects

---
+++ src/PackITPractice.Domain/ValueObjects/PackingItem.cs
@@ -0,0 +1,21 @@
+﻿using PackITPractice.Domain.Exceptions;
+
+namespace PackITPractice.Domain.ValueObjects
+{
+    public record PackingItem
+    {
+        public string Name { get; }
+        public uint Qantity { get; }
+        public bool IsPacked { get; }
+
+        public PackingItem(string name, uint qantity, bool isPacked)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new EmptyPackingListItemNameException();
+
+            Name = name;
+            Qantity = qantity;
+            IsPacked = isPacked;
+        }
+    }
+}
